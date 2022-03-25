682222955e8dec0d88d74dc379530583929afded
Kelvin Tay <kelvintay@Kelvins-MacBook-Pro.local>
2022-03-25 08:20:00 +0800

    push env file

---
+++ .env.ts
@@ -0,0 +1,11 @@
+let globalsettings;
+
+globalsettings = {
+  API_KEY : 'REPLACE_THE_API_KEY',
+  API_SECRET : 'REPLACE_THE_SECRET'
+}
+
+export default {
+  API_KEY : globalsettings.API_KEY,
+  API_SECRET : globalsettings.API_SECRET
+}
